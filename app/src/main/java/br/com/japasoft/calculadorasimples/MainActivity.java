e7580093341cd105c228dda039150d47f318b235
jair.paiva <jair.paiva@prognus.com.br>
2022-12-29 08:45:50 -0300

    Initial commit

---
+++ app/src/main/java/br/com/japasoft/calculadorasimples/MainActivity.java
@@ -0,0 +1,81 @@
+package br.com.japasoft.calculadorasimples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Double num1, num2, res;
+    TextView txtResultant;
+    EditText edtValor1;
+    EditText edtValor2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+/*        Button btnSomar = findViewById(R.id.btnSomar);
+        Button btnSubtracao = findViewById(R.id.btnSubtrair);
+        Button btnMultiplicar = findViewById(R.id.btnMultiplicar);
+        Button btnDividir = findViewById(R.id.btnDividir);
+
+ */
+
+        edtValor1 = findViewById(R.id.edtValor1);
+        edtValor2 = findViewById(R.id.edtValor2);
+        txtResultant = findViewById(R.id.txtResultado);
+    }
+
+    public void somar() {
+        num1 = Double.parseDouble(edtValor1.getText().toString());
+        num2 = Double.parseDouble(edtValor2.getText().toString());
+        res = num1 + num2;
+        txtResultant.setText(String.valueOf(res));
+    }
+
+    public void subtrair() {
+        num1 = Double.parseDouble(edtValor1.getText().toString());
+        num2 = Double.parseDouble(edtValor2.getText().toString());
+        res = num1 - num2;
+        txtResultant.setText(String.valueOf(res));
+    }
+
+    public void multiplicar() {
+        num1 = Double.parseDouble(edtValor1.getText().toString());
+        num2 = Double.parseDouble(edtValor2.getText().toString());
+        res = num1 * num2;
+        txtResultant.setText(String.valueOf(res));
+    }
+
+    public void dividir() {
+        num1 = Double.parseDouble(edtValor1.getText().toString());
+        num2 = Double.parseDouble(edtValor2.getText().toString());
+        res = num1 / num2;
+        txtResultant.setText(String.valueOf(res));
+    }
+
+    public void operacao(View v) {
+        switch (v.getId()) {
+            case R.id.btnSomar:
+                somar();
+                break;
+            case R.id.btnSubtrair:
+                subtrair();
+                break;
+            case R.id.btnMultiplicar:
+                multiplicar();
+                break;
+            case R.id.btnDividir:
+                dividir();
+                break;
+        }
+    }
+
+
+}
